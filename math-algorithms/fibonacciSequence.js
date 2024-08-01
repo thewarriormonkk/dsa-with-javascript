5344278ed1070b30ee3671ed6debbffd58e04e19
Satendra Mishra <msatendra424@gmail.com>
2024-08-01 23:04:01 +0530

    fibonacci sequence

---
+++ math-algorithms/fibonacciSequence.js
@@ -0,0 +1,11 @@
+// fibonacci sequence
+
+function fibonacci(n) {
+    const fib = [0, 1]
+    for (let i = 2; i < n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2]
+    }
+    return fib
+}
+
+// Big-O : O(n)
