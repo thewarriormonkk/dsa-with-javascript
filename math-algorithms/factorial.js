a770a237277b84e6eeac6a8d28a3f195e92492ea
Satendra Mishra <msatendra424@gmail.com>
2024-08-03 23:01:26 +0530

    time complexity of factorial program

--- math-algorithms/factorial.js
+++ math-algorithms/factorial.js
@@ -1,3 +1,5 @@
+// factorial
+
 function factorial(n) {
     let fact = 1;
     for (let i = 2; i <= n; i++) {
@@ -8,3 +10,5 @@ function factorial(n) {
 
 console.log(factorial(5));
 console.log(factorial(1));
+
+// Big-O : O(n)
