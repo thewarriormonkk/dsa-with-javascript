7faea672e954aa6d3ee364229a94b184e2122156
Satendra Mishra <msatendra424@gmail.com>
2024-08-04 16:17:41 +0530

    prime number

---
+++ math-algorithms/primeNumber.js
@@ -0,0 +1,16 @@
+// prime number
+
+function isPrime(n) {
+    if (n < 2) return false;
+    for (let i = 2; i < n; i++) {
+        if (n % i === 0) return false;
+    }
+    return true;
+}
+
+console.log(isPrime(1));
+console.log(isPrime(5));
+console.log(isPrime(7));
+console.log(isPrime(14));
+
+// Time Complexity: O(n)
